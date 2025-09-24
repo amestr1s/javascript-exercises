d0ae6d502e630c1b574d07bffc6c515540c5f329
amestr1s <230774380+amestr1s@users.noreply.github.com>
2025-09-24 17:58:00 +0300

    Ex 11 complete

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,8 +1,12 @@
-const convertToCelsius = function() {
-};
+const convertToCelsius = function(num) {
+  num = ((num - 32) * (5 / 9));
+ return num = Math.round(num * 10) / 10;
+}
 
-const convertToFahrenheit = function() {
-};
+const convertToFahrenheit = function(num) {
+  num = (num * (9 / 5) + 32);
+ return num = Math.round(num * 10) / 10;
+}
 
 // Do not edit below this line
 module.exports = {
