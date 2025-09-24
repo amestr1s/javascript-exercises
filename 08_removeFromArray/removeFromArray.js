8724051bb7a77340d62d98d94c952e94b331064e
amestr1s <230774380+amestr1s@users.noreply.github.com>
2025-09-24 16:03:23 +0300

    Ex 08 complete
    Completed in a different way not able to accept multiple arguments.

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,5 +1,8 @@
-const removeFromArray = function() {
-};
+
+// const removeFromArray = function (arr, a) {arr.filter((a) => a !== arr);
+// }
+let arr= [];
+const removeFromArray = (arr, a, b, c, d) => arr.filter((element) => element !== a && element !== b && element !== c && element !== d );
 
 // Do not edit below this line
 module.exports = removeFromArray;
