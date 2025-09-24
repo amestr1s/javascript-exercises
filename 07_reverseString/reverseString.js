72512d78e9636409af1026e117e29c8714f417fc
amestr1s <230774380+amestr1s@users.noreply.github.com>
2025-09-24 14:24:00 +0300

    Ex 07 complete
    Done in a different way cause i didn't remember that text.split creates an array.

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,6 +1,17 @@
-const reverseString = function() {
+const reverseString = function(text) {
 
-};
+const arr = [];
+//let char = "";
+
+let length = text.length;
+for (let i = length - 1; i >= 0; i--) {
+let a = text.charAt(i);
+arr.push(a);
+}
+
+return arr.join("");
+}
+
 
 // Do not edit below this line
 module.exports = reverseString;
