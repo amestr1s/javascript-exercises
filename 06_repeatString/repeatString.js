399828b01f027142b273e127c5d4f3e7ef62a1c7
amestr1s <230774380+amestr1s@users.noreply.github.com>
2025-09-24 13:10:50 +0300

    Ex 06 complete

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,6 +1,28 @@
-const repeatString = function("hey", b) => {
-    
-};
+
+const repeatString = function(string, num) { 
+    if (num >= 0) {
+    let string2 = "";
+    for (let i = 1; i <= num; i++) {
+    string2 += string;
+    } return string2;
+} else return string2 = "ERROR";
+}
+
+
+
+
+
+
+// heyheyheyheyheyheyheyheyhey
+//heyheyhey
+
+
+
+
+
+
+
+
 
 // Do not edit below this line
 module.exports = repeatString;
