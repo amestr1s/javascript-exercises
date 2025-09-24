6e0cda67f0f421c36b8fbcef374c60d8ecfa442c
amestr1s <230774380+amestr1s@users.noreply.github.com>
2025-09-24 17:11:57 +0300

    Ex 09 complete
    Ex 09 done completely differently with arrays and it's working properly.

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,6 +1,50 @@
-const sumAll = function() {
+// function sum(...numbers) {
+//         let result = 0;
+//         for(let number of numbers){
+//             if (number < 0) {
+//                 result = "ERROR";
+//                 return result;
+//             }else {
+//             result += number;
+//         } 
+//         return result;
+//     }
+// }
 
-};
+// const sumAll = function sum(...numbers) {
+//         let result = 0;
+//         for(let number of numbers){
+//             if (number < 0) {
+//                 result = "ERROR";
+//                 return result;
+//             }else {
+//             result += number;
+//         } 
+//         return result;
+//     }
+// }
+// const arr = [];
+// function getInts (a, b) {
+//     for (let i = a; i <= b; i++ ) {
+//         arr.push(i);
+    
+//     } return arr;
+// }
+
+const sumAll = function getInts (a, b) {
+    const arr = [];
+    if (a < b && a > 0 && b > 0 && Number.isInteger(a) && Number.isInteger(b)) {
+    for (let i = a; i <= b; i++ ) {
+        arr.push(i);
+    
+    } return arr.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
+} else if (b < a && a > 0 && b > 0 && Number.isInteger(a) && Number.isInteger(b)) {
+    for (let i = b; i <= a; i++ ) {
+        arr.push(i);
+    
+    } return arr.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
+ } else return str = "ERROR";
+}
 
 // Do not edit below this line
 module.exports = sumAll;
