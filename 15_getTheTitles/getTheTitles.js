b9a67071d8e17524c29224a3b8a1ec000457fb7e
amestr1s <230774380+amestr1s@users.noreply.github.com>
2025-09-27 16:19:13 +0300

    Ex 15 completed

--- 15_getTheTitles/getTheTitles.js
+++ 15_getTheTitles/getTheTitles.js
@@ -1,5 +1,6 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(books) {
+    let getTitles = books.map(item => item.title);
+    return getTitles;
 };
 
 // Do not edit below this line
