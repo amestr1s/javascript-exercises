85ec3ea444249229004b34156922a0dc2743432a
amestr1s <230774380+amestr1s@users.noreply.github.com>
2025-09-27 14:05:11 +0300

    Ex 12 complete

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,35 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	let result = arr.reduce((sum, current) => sum + current, 0);
+  return result;
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  let result = arr.reduce((product, current) => product * current);
+  return result;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+	if (a === 0) {
+    return result = 1;
+  } else 
+    result = a;
+    let c = 1;
+    for (let i = 0; i < (a - 1); i++) {
+      result *= (a - c);
+      c++;
+    } return result;
 };
 
 // Do not edit below this line
