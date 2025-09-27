c3782c9df7d7e9b6a24118e02f9b3c1e283f821f
amestr1s <230774380+amestr1s@users.noreply.github.com>
2025-09-27 15:01:17 +0300

    Ex 13 completed differently

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,6 +1,59 @@
-const palindromes = function () {
+function noSpace(value) {
+    return value !== " " && value !== "!" && value !== "." && value !== ",";
+}
 
+const palindromes = function (text) {
+    let text2 = text.toLowerCase();
+    let textSplit = text2.split("");
+    let filteredArr = textSplit.filter(noSpace);
+    let text3 = filteredArr.toString();
+    let reversedArr = filteredArr.reverse();
+    let text4 = reversedArr.toString();
+    if (text3 === text4) {
+        return true;
+    } else return false;
 };
+
+
+
+
+
+
+// // Create and display an array:
+// const fruits = ["Banana", "Orange", "Apple", "Mango"];
+// document.getElementById("demo1").innerHTML = fruits;
+
+// // First sort the array
+// fruits.sort();
+
+// // Then reverse it:
+// fruits.reverse();
+
+// document.getElementById("demo2").innerHTML = fruits;
+
+
+
+// let text = "How are you doing today?";
+// const myArray = text.split("");
+
+// document.getElementById("demo").innerHTML = myArray;
+
+
+
+// const numbers = ["la", "three",  ,  , 25];
+// const over18 = numbers.filter(myFunction);
+
+// document.getElementById("demo").innerHTML = over18;
+
+// function myFunction(value) {
+//   return value !== " ";
+// }
+
+
+
+
+
+
 
 // Do not edit below this line
 module.exports = palindromes;
