a7cf67a9863f8a67bbafd183fad23a17e0c50b53
amestr1s <230774380+amestr1s@users.noreply.github.com>
2025-09-27 16:09:23 +0300

    Ex 14 completed differently

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,6 +1,18 @@
-const fibonacci = function() {
+const fibonacci = function(numMax){
+    if (numMax === 0 || numMax === "0") {
+        return 0;
+    } else if (numMax < 0) {
+        return "OOPS";
+    } else {
+    let fibArray = [1];
+    let i = 0, j = 1, x;
+    for(let k = 0; k < numMax; i = j, j = x, k++) {
+        x=i+j;
+        fibArray.push(x);
+    } return fibArray[numMax - 1];
+    }}
 
-};
+
 
 // Do not edit below this line
 module.exports = fibonacci;
